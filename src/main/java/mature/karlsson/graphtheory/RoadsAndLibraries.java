2adf0d98954b846664ee1bd83121fbbcf9fa812a
Alex <alexander.levin24@gmail.com>
2018-02-18 19:13:00 +1000

    road and librariies - WIP

--- src/main/java/mature/karlsson/graphtheory/RoadsAndLibraries.java
+++ src/main/java/mature/karlsson/graphtheory/RoadsAndLibraries.java
@@ -7,11 +7,9 @@ import java.util.*;
 public class RoadsAndLibraries {
 
     private int q, n, m, clib, croad = 0;
-    //every integer in the array is a city (adjMatrix[0] is city #1)
-    //every bit in an integer is accessibility from the city to another one (bit 0 set to 1 means city #1 is accessible from this one)
-    private int[] adjMatrix;
-    //every bit in the integer means city has been visited or not (bit 0 set to 1 means city #1 has been visited)
-    private int visitedCities;
+    private List<Set<Integer>> graph;
+
+    private boolean[] visitedCities;
 
     public static void main(String... args) throws FileNotFoundException {
         if (args.length > 0) {
@@ -25,49 +23,32 @@ public class RoadsAndLibraries {
     private void process() {
         Scanner in = new Scanner(System.in);
         q = in.nextInt();
-        for (int i=1; i<=q; i++) {
-            initQuery(in);
-            long theCost;
 
-            if (clib <= croad) {
-                //just build a library in each city if libraries are cheaper then roads
-                theCost = (long)clib * (long)n;
-            } else {
-                Collection<Integer> connectedGraphs = new ArrayList<>();
-                for (int cnum = 0; cnum < n; cnum++)
-                    if (!visitedCities[cnum])
-                        connectedGraphs.add(bfs(cnum));
-                theCost = connectedGraphs.size()*clib; //one library per segment
-                for (int ccnt : connectedGraphs)
-                    theCost += (long)(ccnt-1)*(long)croad; //road for every city in segment without library
+        int connectedComponentsNum = 0;
+        for (int qi=1; qi<=q; qi++) {
+            initQuery(in);
+            for (int ci = 0; ci < n; ci++) {
+                //if a city has not been visited yet then build a connected component from it
+                dfsTraversal(ci);
+                connectedComponentsNum++;
             }
 
-            System.out.println(theCost);
+            //if a library costs less than a road then just build a library in each of the graph's connected components
+            //otherwise build a single library (in one of the segments) and link the remaining with a road
+            int cost = clib <= croad ?
+                    connectedComponentsNum*clib :
+                    clib + (connectedComponentsNum-1)*croad;
+            System.out.println(cost);
         }
     }
 
-
-    private int bfs(int cstart) {
-        Queue<Integer> queue = new ArrayDeque<>(n);
-        queue.add(cstart);
-        addVisitedCity(cstart);
-        int ctot = 0;
-
-        while (queue.peek() != null) {
-            int cnum = queue.remove();
-            ctot++;
-            for (int i = 0; i < n; i++)
-                if (adjMatrix[cnum][i] && !visitedCities[i]) {
-                    queue.add(i);
-                    visitedCities[i] = true;
-                }
+    private void dfsTraversal(int city) {
+        if (!visitedCities[city]) {
+            visitedCities[city] = true;
+            for (Integer adjCity : graph.get(city)) {
+                dfsTraversal(adjCity);
+            }
         }
-
-        return ctot;
-    }
-
-    private void addVisitedCity(int cnum) {
-        visitedCities = visitedCities | 1 << (cnum - 1);
     }
 
     private void initQuery(Scanner in) {
@@ -76,16 +57,19 @@ public class RoadsAndLibraries {
         clib = in.nextInt(); //cost of a library
         croad = in.nextInt(); //cost of a road
 
-        adjMatrix = new int[n];
+        graph = new ArrayList<>(n);
+        for (int i = 0; i < n; i++) {
+            graph.add(new HashSet<>());
+        }
+
         for (int i = 1; i <= m; i++) {
             int ui = in.nextInt();
             int vi = in.nextInt();
-            adjMatrix[ui] = adjMatrix[ui] | 1 << (vi - 1);
-            adjMatrix[vi] = adjMatrix[vi] | 1 << (vi - 1);
+            graph.get(ui-1).add(vi-1);
+            graph.get(vi-1).add(ui-1);
         }
 
-        visitedCities = 0;
+        visitedCities = new boolean[n];
     }
-
 
 }
